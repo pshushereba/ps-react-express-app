23bf6cf178e20737170f2705ebf36df5886aa4ba
Patrick Shushereba <pshushereba@gmail.com>
2020-10-15 11:19:19 -0700

    Added redux and basic react components.

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+import { store } from "./store";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Main } from "./components/Main.js";
+
+ReactDOM.render(<Main />, document.getElementById("app"));
+
+console.log(store.getState());
